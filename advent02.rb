1e6ff043114debe064c1ee65c74b03bce34fdc89
Alison Kaufman <alisonkaufman@Alisons-MacBook-Air.local>
2017-12-03 01:09:45 -0800

    Day 2

---
+++ advent02.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+
+# DATA = gets.chomp
+
+module Day2
+	def solve_first( input_file )
+		sum = 0
+
+		File.open( input_file, "r" ) do |f|
+  			f.each_line do |line|
+    			num_arr = line.split.map( &:to_i )
+    			min = num_arr.min
+    			max = num_arr.max
+    			sum += max - min
+    		end
+  		end
+
+  		sum
+	end
+	
+	module_function :solve_first
+end
+
+RSpec.describe( Day2 ) do
+
+	it "Part 1" do
+		expect( Day2.solve_first( 'test_data.txt' ) ).to eq( 18 )
+	end
+
+end
+
+# puts Day2.solve_first( DATA )
